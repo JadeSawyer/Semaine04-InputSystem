f6b990b521a95e1111088d405cb61f9c072522a7
JadeSawyer <chen.pinyu@hotmail.com>
2024-02-16 10:57:00 -0500

    Mouvement Vaisseau

--- Assets/Scripts/Vaisseau.cs
+++ Assets/Scripts/Vaisseau.cs
@@ -10,10 +10,12 @@ public class Vaisseau : MonoBehaviour
     
     private Vector2 _mouvementHorizontal;
 
+    private Rigidbody _rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,11 +26,20 @@ public class Vaisseau : MonoBehaviour
 
     public void RecevoirInfo(InputAction.CallbackContext context)
     {
-        Debug.Log(context.ReadValue<Vector2>()); // (1,0) (-1,0) (0,0)
+        //Debug.Log(context.ReadValue<Vector2>()); // (1,0) (-1,0) (0,0)
+        _mouvementHorizontal = context.ReadValue<Vector2>();
+
+        _valeurX = _mouvementHorizontal.x;
+        _valeurY = _mouvementHorizontal.y;
     }
 
-    void Bouge()
+    public void Bouge()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime);
+        Vector2 _mouvement = new Vector2(_valeurX, _valeurY);
+
+        _rb.AddForce(_mouvement * 40, ForceMode.Force);
+
+        //_rb.AddForce(_mouvementHorizontal, ForceMode.Force);
+
     }
 }
